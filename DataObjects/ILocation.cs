f8b1987334beb0d27f3d5fc2120cfcab8901fb75
TomSavas <tomas.savickas@protonmail.com>
2019-02-19 19:33:03 +0000

    Add id to a location, fix distance calculation

--- DataObjects/ILocation.cs
+++ DataObjects/ILocation.cs
@@ -4,12 +4,11 @@ namespace Krafi.DataObjects
 {
     public interface ILocation 
     {
+        string Id { get; }
         string Alias { get; }
         double Longitude { get; }
         double Latitude { get; }
 
         double DistanceInMetres(ILocation otherLocation);
-        double Distance(ILocation otherLocation);
-        double DistanceSquared(ILocation otherLocation);
     }
 }
--- DataObjects/Location.cs
+++ DataObjects/Location.cs
@@ -4,14 +4,19 @@ namespace Krafi.DataObjects
 {
     public class Location : ILocation
     {
-        private const double DEG_TO_METRES = 10000;
+        private const double _equatorialEarthRadius = 6378.1370D;
+        private const double _degreesToRadians = (Math.PI / 180D);
 
+        public string Id { get; }
         public string Alias { get; }
         public double Longitude { get; }
         public double Latitude { get; }
 
-        public Location(string alias, double longitude, double latitude) 
+        public Location() : this("", "", 0d, 0d) {}
+
+        public Location(string id, string alias, double longitude, double latitude) 
         {
+            Id = id;
             Alias = alias;
             Longitude = longitude;
             Latitude = latitude;
@@ -19,19 +24,25 @@ namespace Krafi.DataObjects
 
         public double DistanceInMetres(ILocation otherLocation) 
         {
-            return DEG_TO_METRES * Distance(otherLocation);
+            return HaversineInM(Latitude, Longitude, otherLocation.Latitude, otherLocation.Longitude);
         }
 
-        public double Distance(ILocation otherLocation) 
+        private int HaversineInM(double lat1, double long1, double lat2, double long2)
         {
-            return Math.Sqrt(DistanceSquared(otherLocation));
+            return (int)(1000d * HaversineInKM(lat1, long1, lat2, long2));
         }
 
-        public double DistanceSquared(ILocation otherLocation) 
+        // Thanks to this chap: https://stackoverflow.com/a/7595937
+        private double HaversineInKM(double lat1, double long1, double lat2, double long2)
         {
-            // Inaccurate because of the curvature of the Earth, however, the distances we are concerned with are too
-            // small to be affected by this. Worth the consideration, but efficiency implications must be investigated.
-            return Math.Pow(Longitude - otherLocation.Longitude, 2) + Math.Pow(Latitude - otherLocation.Latitude, 2);
+            double dlong = (long2 - long1) * _degreesToRadians;
+            double dlat = (lat2 - lat1) * _degreesToRadians;
+            double a = Math.Pow(Math.Sin(dlat / 2d), 2d) + Math.Cos(lat1 * _degreesToRadians) * Math.Cos(lat2 * _degreesToRadians) * Math.Pow(Math.Sin(dlong / 2d), 2d);
+            double c = 2D * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1D - a));
+            double d = _equatorialEarthRadius * c;
+
+            return d;
         }
+
     }
 }

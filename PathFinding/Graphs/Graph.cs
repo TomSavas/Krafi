88f306df93124588dcbd1e23bf98535b245601e3
TomSavas <tomas.savickas@protonmail.com>
2019-02-19 20:02:11 +0000

    Add simple graph implementation

---
+++ PathFinding/Graphs/Graph.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace Krafi.PathFinding.Graphs
+{
+    public class Graph<T> : IGraph<T> where T : INode
+    {
+        public Dictionary<string, T> Nodes { get; }
+
+        public Graph()
+        {
+            Nodes = new Dictionary<string, T>();
+        }
+
+        public void Reset() 
+        {
+            foreach(var node in Nodes.Values)
+            {
+                node.FastestTransit = null;
+                node.ArrivalTime = new TimeSpan();
+                node.DepartureTime = new TimeSpan();
+
+                foreach(var transit in node.Transits)
+                {
+                    transit.ArrivalTime = new TimeSpan();
+                    transit.DepartureTime = new TimeSpan();
+                }
+            }
+        }
+    }
+}

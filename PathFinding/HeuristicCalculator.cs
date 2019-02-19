2fd38df412e2a4158bbae26bcc625ab4b95e7b0b
TomSavas <tomas.savickas@protonmail.com>
2019-02-19 19:42:39 +0000

    Change how heuristic is calculated

--- PathFinding/HeuristicCalculator.cs
+++ PathFinding/HeuristicCalculator.cs
@@ -1,5 +1,6 @@
 using System;
 using Krafi.DataObjects;
+using Krafi.DataObjects.Vehicles;
 
 namespace Krafi.PathFinding 
 {
@@ -7,7 +8,10 @@ namespace Krafi.PathFinding
     {
         public double CalculateWeight(ITransit transit, TimeSpan departureTime) 
         {
-            return transit.StartNode.Location.Distance(transit.EndNode.Location);
+            // Make sure, that the heuristic weight is always lower than any possible path from startNode to endNode.
+            // This isn't a bulletproof way to ensure that, however, having an entity moving 10 times the speed of
+            // a person seems reasonable enough, to say that it's going to be faster than any possible path.
+            return new Feet().TravelTime(transit.StartNode.Location, transit.EndNode.Location, departureTime).TotalMinutes / 10;
         }
     }
 }

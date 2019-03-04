21ff86ce381a2adb74914aeda7274fd3463cb0e2
TomSavas <tomas.savickas@protonmail.com>
2019-03-04 20:16:44 +0000

    Remove broken transfer penalising system

--- PathFinding/PenalisingWeightCalculator.cs
+++ PathFinding/PenalisingWeightCalculator.cs
@@ -5,17 +5,15 @@ namespace Krafi.PathFinding
 {
     public class PenalisingWeightCalculator : WeightCalculatorWrapper 
     {
-        private const double WALKING_PENALTY = 3;
-        private const double TRANSFER_PENALTY = 0;//20;
+        private const double WALKING_PENALTY = 5;
 
         public PenalisingWeightCalculator(IWeightCalculator _baseWeightCalculator) : base(_baseWeightCalculator) {}
 
         public override double CalculateWeight(ITransit transit, TimeSpan departureTime)
         {
             var transportTypePenalty = transit.Transport.Alias == "Feet" ? WALKING_PENALTY : 0d;
-            var transferPenalty = transit.StartNode.FastestTransit == null || transit.StartNode.FastestTransit.Transport.Alias == transit.Transport.Alias ? 0 : TRANSFER_PENALTY;
 
-            return base.CalculateWeight(transit, departureTime) + transportTypePenalty + transferPenalty;
+            return base.CalculateWeight(transit, departureTime) + transportTypePenalty;
         }
     }
 }
